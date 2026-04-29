68555c02647c90c68f601a52bc3cee17840f5809
Abcde Austria <keeplogged@gmail.com>
2026-04-30 00:03:19 +0800

    fix: resolve Sanctum NewAccessToken type-hint conflict

--- app/Models/MongoPersonalAccessToken.php
+++ app/Models/MongoPersonalAccessToken.php
@@ -2,11 +2,14 @@
 
 namespace App\Models;
 
-use MongoDB\Laravel\Eloquent\Model;
-use Laravel\Sanctum\Contracts\HasAbilities;
+use Laravel\Sanctum\PersonalAccessToken as BaseToken;
+use MongoDB\Laravel\Eloquent\HybridRelations;
 
-class MongoPersonalAccessToken extends Model implements HasAbilities
+class MongoPersonalAccessToken extends BaseToken
 {
+    use HybridRelations;
+
+    protected $connection = 'mongodb';
     protected $collection = 'personal_access_tokens';
     protected $keyType = 'string';
     public $incrementing = false;
@@ -20,34 +23,4 @@ class MongoPersonalAccessToken extends Model implements HasAbilities
         'tokenable_id',
         'tokenable_type',
     ];
-
-    protected $hidden = [
-        'token',
-    ];
-
-    protected $casts = [
-        'abilities' => 'array',
-        'last_used_at' => 'datetime',
-        'expires_at' => 'datetime',
-    ];
-
-    public function tokenable()
-    {
-        return $this->morphTo();
-    }
-
-    public function can($ability)
-    {
-        return in_array('*', $this->abilities) || in_array($ability, $this->abilities);
-    }
-
-    public function canAny($abilities)
-    {
-        return in_array('*', $this->abilities) || array_intersect($abilities, $this->abilities);
-    }
-
-    public function cant($ability)
-    {
-        return !$this->can($ability);
-    }
 }
